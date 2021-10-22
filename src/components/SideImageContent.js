84ea22d309d34746fd31524f89b3860ef5d32e07
Milinko <milinko.m.dragovic@gmail.com>
2021-10-22 21:41:34 +0200

    finish side image content - home page

--- src/components/SideImageContent.js
+++ src/components/SideImageContent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Video from '../assets/videos/kompitenz-video.mp4'
 import ImageBg from '../assets/images/slider/hero-1.jpg'
+import SideIcon from '../assets/images/side-widget-icon.png'
 import { AiOutlinePlayCircle } from 'react-icons/ai';
 
 const SideImageContent = () => {
@@ -21,18 +22,20 @@ const SideImageContent = () => {
             </SideImageContentSide>
             <SideImageContentSide>
                 <SideImageContentSideInner>
-                    <SectionTitle>
+                    <div className="section-title">
                         <span>02</span>
-                        <h2>introduction</h2>
-                        <h6>Do this for both directions of traffic if it’s this</h6>
-                    </SectionTitle>
-                    <SectionCounter>
-                            <li> <span class="odometer odometer-auto-theme" id="1"><div class="odometer-inside"><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">1</span></span></span></span></span><span class="odometer-formatting-mark">,</span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">7</span></span></span></span></span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">0</span></span></span></span></span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">0</span></span></span></span></span></div></span><span class="symbol">+</span> <small>Underestate</small> </li>
-                            <li> <span class="odometer odometer-auto-theme" id="2"><div class="odometer-inside"><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">8</span></span></span></span></span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">8</span></span></span></span></span></div></span><span class="symbol">k</span> <small>Underestate</small> </li>
-                            <li> <span class="odometer odometer-auto-theme" id="3"><div class="odometer-inside"><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">9</span></span></span></span></span><span class="odometer-digit"><span class="odometer-digit-spacer">8</span><span class="odometer-digit-inner"><span class="odometer-ribbon"><span class="odometer-ribbon-inner"><span class="odometer-value">6</span></span></span></span></span></div></span><span class="symbol">%</span> <small>Underestate</small> </li>
-                    </SectionCounter>
-                    <p>If you are not actively using <strong>Facebook</strong>, <strong>Twitter</strong> and other social media platforms, take the time to do so. Get into the habit of updating your business page or <u>tweets</u> at the same time every day. </p>
-                    <p>Take advantage of the <u>software</u> that can help turn your blogs into tweets and know when the best time of day is to connect with your base.</p>
+                        <h2><strong>100%</strong> Transparent</h2>
+                        <h6>
+                            Herausforderungen im digitalen Zeitalter Meistern
+                        </h6>
+                    </div>
+                    <SideImageIcon src={SideIcon} />
+                    <p>
+                        Sie gestalten von Anfang an mit und haben volle Einsicht in allen Entwicklungsstufen vom Prototyp (MVP / funktional) bis zum fertigen Produkt.
+                    </p>
+                    <p>
+                        Im Zuge des Entwicklungsprozesses wächst das Verständnis für Ihre Geschäftsprozesse und die Möglichkeit zukünftiger Erweiterungen.
+                    </p>
                 </SideImageContentSideInner>
             </SideImageContentSide>
         </SideImageContentContainer>
@@ -56,7 +59,7 @@ const SideImageContentSide = styled.div`
     padding: 120px 0;
     background-size: cover !important;
     position: relative;
-    background-color: rgb(64, 80, 137);
+    background-color: ${({ theme }) => theme.colors.primary};
 `;
 
 const SideImageContentSideInner = styled.div`
@@ -65,8 +68,24 @@ const SideImageContentSideInner = styled.div`
     margin: 0 auto;
     padding: 0 60px;
 
+    .section-title {
+        h2 {
+            color: ${({ theme }) => theme.colors.white};
+            strong {
+                opacity: .65;
+            }
+        }
+
+        h6 {
+            color: ${({ theme }) => theme.colors.white};
+            line-height: 1.6;
+        }
+    }
+
     p {
-        color: #ffffff;
+        color: ${({ theme }) => theme.colors.white};
+        line-height: 1.6;
+        margin-bottom: 1rem;
     }
 
     img {
@@ -88,7 +107,7 @@ const SideImageContentLeftSideVideo = styled.div`
     -ms-transition: 0.25s ease-in-out;
     -o-transition: 0.25s ease-in-out;
     transition: 0.25s ease-in-out;
-    background: #f65935;
+    background: #945b8e12;
 `;
 
 const LeftSideVideo = styled.video`
@@ -108,51 +127,12 @@ const LeftSideVideoButton = styled.a`
     transition: all 0.5s;
     z-index: 999;
     font-size: 5rem;
-    color: #ffffff;
+    color: ${({ theme }) => theme.colors.white};
 `;
 
-const SectionTitle = styled.div`
-    margin-bottom: 40px;
-    color: #fff;
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: column;
-
-    span {
-        display: block;
-        font-size: 18px;
-        font-weight: 800;
-        color: #fff;
-        opacity: 0.7;
-    }
-
-    h2 {
-        display: block;
-        font-weight: 700;
-        color: #fff;
-        margin-bottom: 20px;
-    }
-
-    h6 {
-        font-size: 19px;
-        color: #fff;
-        line-height: 30px;
-        margin-bottom: 0;
-    }
-`;
-
-const SectionCounter = styled.ul`
-    width: 100%;
-    float: left;
-    margin-bottom: 40px;
-    padding: 0;
-
-    li {
-        width: 33.33333%;
-        float: left;
-        margin: 0;
-        padding: 0;
-        list-style: none;
-    }
+const SideImageIcon = styled.img`
+    height: 5em !important;
+    margin: 0 auto;
+    display: block;
+    margin-bottom: 2rem;
 `;
